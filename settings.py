fb1d7f3c907202907353b24ee0b4b2d936847ff1
Jason Robbins <jrobbins@gmail.com>
2022-08-06 04:40:01 +0000

    Initial setup

---
+++ settings.py
@@ -0,0 +1,9 @@
+import logging
+import os
+
+
+UNIT_TEST_MODE = os.environ['SERVER_SOFTWARE'].startswith('test')
+
+if not UNIT_TEST_MODE:
+  # Py3 defaults to level WARN.
+  logging.basicConfig(level=logging.INFO)
