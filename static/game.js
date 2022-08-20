3db64466f927f4b8cd75ab31a27f39d60c5f06fc
Jason Robbins <jrobbins@gmail.com>
2022-08-20 17:33:06 +0000

    Ready to start client-side development

---
+++ static/game.js
@@ -0,0 +1,5 @@
+const nickEl = document.querySelector('#nick');
+const joinEl = document.querySelector('#join');
+const canvasEl = document.querySelector('canvas');
+
+console.log('this is game.js')
